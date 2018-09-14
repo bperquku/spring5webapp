726d7f87bdc27e193dd07f4f7bcc89a680713e70
bperquku <bperquku@gmail.com>
2018-09-14 09:55:03 +0200

    JPA

--- spring5webapp/src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -3,10 +3,22 @@ package guru.springframework.spring5webapp.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class Author {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
 	private String firstName;
 	private String lastName;
-
+	
+	@ManyToMany(mappedBy="authors")
 	private Set<Book> books = new HashSet<>();
 
 	public Author() {
@@ -21,6 +33,14 @@ public class Author {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.books = books;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return this.id;
 	}
 
 	public String getFirstName() {
@@ -45,5 +65,5 @@ public class Author {
 
 	public void setBooks(Set<Book> books) {
 		this.books = books;
-	}
+	}	
 }
--- spring5webapp/src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -3,59 +3,81 @@ package guru.springframework.spring5webapp.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 
 	private String title;
-    private String isbn;
-    private String publisher;
-    
-    private Set<Author> authors = new HashSet<>();
-    
-    public Book() {
-    }
+	private String isbn;
+	private String publisher;
 
-    public Book(String title, String isbn, String publisher) {
-        this.title = title;
-        this.isbn = isbn;
-        this.publisher = publisher;
-    }
+	@ManyToMany
+	@JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
+	private Set<Author> authors = new HashSet<>();
 
-    public Book(String title, String isbn, String publisher, Set<Author> authors) {
-        this.title = title;
-        this.isbn = isbn;
-        this.publisher = publisher;
-        this.authors = authors;
-    }
+	public Book() {
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public Book(String title, String isbn, String publisher) {
+		this.title = title;
+		this.isbn = isbn;
+		this.publisher = publisher;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public Book(String title, String isbn, String publisher, Set<Author> authors) {
+		this.title = title;
+		this.isbn = isbn;
+		this.publisher = publisher;
+		this.authors = authors;
+	}
 
-    public String getIsbn() {
-        return isbn;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
+	public Long getId() {
+		return this.id;
+	}
 
-    public String getPublisher() {
-        return publisher;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public Set<Author> getAuthors() {
-        return authors;
-    }
+	public String getIsbn() {
+		return isbn;
+	}
 
-    public void setAuthors(Set<Author> authors) {
-        this.authors = authors;
-    }
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public Set<Author> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(Set<Author> authors) {
+		this.authors = authors;
+	}
 }
